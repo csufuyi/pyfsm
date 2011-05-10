8635ea32048c582de0c794e9733238664a9db02e
jsternberg <jonathansternberg@gmail.com>
2011-05-10 02:13:07 -0400

    setup script for building/installing pyfsm.

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from distutils.core import setup
+
+setup(name='pyfsm',
+      version='1.0',
+      description='Python Finite State Machine',
+      author='Jonathan Sternberg',
+      author_email='jonathansternberg@gmail.com',
+      url='http://code.google.com/p/pyfsm',
+      py_modules=['pyfsm']
+      )
